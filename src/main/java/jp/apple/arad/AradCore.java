2c98e487e3162e7ada932f25489cdb905e79bc70
アップルパイ <sou.120724.s@gmail.com>
2026-07-30 19:07:30 +0900

    chore:version

--- src/main/java/jp/apple/arad/AradCore.java
+++ src/main/java/jp/apple/arad/AradCore.java
@@ -26,7 +26,7 @@ import org.apache.logging.log4j.Logger;
 
 import static jp.apple.AppleLib.tabAppleLib;
 
-@Mod(modid = "arad", name = "ARAD", version = "1.1.2", dependencies = "required-after:rtm")
+@Mod(modid = "arad", name = "ARAD", version = "1.2.0", dependencies = "required-after:rtm")
 
 public class AradCore {
 
